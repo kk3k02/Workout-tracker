2d654973d85d3c4ef4796cf72777cdc817a2c0d8
kk3k02 <kubajak@outlook.com>
2024-10-21 15:20:05 +0200

    Removed arms from enum

--- app/src/main/java/com/kk3k/workouttracker/db/TargetMuscle.kt
+++ app/src/main/java/com/kk3k/workouttracker/db/TargetMuscle.kt
@@ -7,7 +7,6 @@ enum class TargetMuscle {
     SHOULDERS, // Exercises for shoulders
     CHEST, // Exercises for chest
     BACK, // Exercises for back
-    ARMS, // Exercises for arms
     FOREARMS, // Exercises for forearms
     ABS, // Exercises for abs
     LEGS, // Exercises for legs
